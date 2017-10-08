198b7730e9e9fd6a8945c0af5d1a2997092bbd58
LinchSmyth <andrei95-1995@mail.ru>
2017-10-08 19:29:16 +0300

    add all necessary styles/fonts for vuetify | configure some vue stuff

---
+++ app/javascript/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+  import Vue         from 'vue'
+  import Vuetify     from 'vuetify'
+  import VueResource from 'vue-resource'
+
+  Vue.use(Vuetify)
+  Vue.use(VueResource)
+
+
+  export default {
+    name: 'app'
+  }
+</script>
