9b7f8871bd83cb168559d2623874a7efcac48ab5
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-11-27 12:59:00 +0200

    update with permit params

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,29 +7,14 @@ class LessonsController < ApplicationController
       return
     end
     lessons = group.lessons
-    is_group_by_weeks = params[:group_by_weeks] == "true"
-
-    if params.key?(:from) && params.key?(:to)
-      custom_serialization(lessons, is_group_by_weeks, "accept \'from\' and \'to\' params")
-    elsif params.key?(:from)
-      custom_serialization(lessons, is_group_by_weeks, "accept only \'from\' param")
-    elsif params.key?(:to)
-      custom_serialization(lessons, is_group_by_weeks, "accept only \'to\' param")
-    elsif params.key?(:week)
-      if params[:week].count == 1
-        custom_serialization(lessons, is_group_by_weeks, "accept #{params[:week].first} week")
-      elsif params[:week].count == 2
-        custom_serialization(lessons, is_group_by_weeks, "accept #{params[:week].first}-#{params[:week].last} week")
-      end
-    end
   end
 
 
   private
 
 
-  def custom_serialization(lessons, is_group_by_weeks, accept_method)
-    render json: { group_by_weeks: is_group_by_weeks, custom_serialization: accept_method, lessons: lessons }
+  def lesson_params
+    params.permit(:from, :to, :group_by_weeks, week: [])
   end
 
 end
